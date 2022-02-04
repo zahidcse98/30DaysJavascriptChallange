cbce5b6b8c07cc5b19f6c09538bed8b37a3cd667
Zahid Hasan <zahidcse98@gmail.com>
2022-02-05 02:36:05 +0600

    challenge 9 completed successfully

---
+++ 9_14MustKnowDevToolsTricks/script.js
@@ -0,0 +1,32 @@
+const dogs = [{name: 'Snickers', age: 2}, {name: 'hugo', age: 8}];
+const p = document.querySelector('p');
+function makeGreen() {
+    
+    p.style.color = '#BADA55';
+    p.style.fontSize = '50px';
+}
+
+    // Regular
+    console.log('Hello World');
+    // Interpolated
+    console.log('Hello I am %s Student!', 'CSE');
+    // Styled
+    console.log('%c Zahid Hasan Aalif', 'font-size:60px; color:red; ');
+    // warning
+    console.warn("WTF");
+    // Error :|
+    console.error("Fuck y0u!")
+    // Info
+    console.info("You are single!");
+    // Testing
+    console.log(1 === 2, 'WRONG!!')
+    // clearing
+    console.clear();
+    // Viewing DOM elements
+    console.log(p);
+    console.dir(p);
+    // Grouping together
+
+    // counting
+
+    // timing
