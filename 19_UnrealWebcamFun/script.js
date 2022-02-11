5b54785733f38e1a5b9755f96fc9424bdca6232a
Zahid Hasan <zahidcse98@gmail.com>
2022-02-11 22:10:36 +0600

    challenge 19 successfully completed

---
+++ 19_UnrealWebcamFun/script.js
@@ -0,0 +1,62 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+        .then(localMediaStream => {
+           // console.log(localMediaStream);
+            video.srcObject = localMediaStream;
+            video.play();
+        })
+        .catch(err => {
+            console.error(`OH NO!!!`, err);
+        })
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    //console.log(width, height);
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0,0, width, height);
+        // take the pixels out
+        let pixels = ctx.getImageData(0,0,width, height);
+        // mess with them
+        pixels = redEffect(pixels);
+        // put them back
+        ctx.putImageData(pixels, 0,0);
+    }, 16);
+}
+
+function takePhoto() {
+
+    //play the sound
+    snap.currentTime = 0;
+    snap.play();
+
+    //take the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+    strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+    for(let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 100; //RED
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; //GREEN
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5 //Blue
+    }
+    return pixels;
+}
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
